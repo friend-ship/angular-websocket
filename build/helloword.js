1a2b3658f6c9a39e1a41730423aa33a8075a144f
li_xiaoteng <li_xiaoteng@uxsino.com>
2018-07-13 13:53:44 +0800

    添加注释

--- build/helloword.js
+++ build/helloword.js
@@ -4,7 +4,9 @@ var ws_1 = require("ws");
 var wsServer = new ws_1.Server({ port: 8081 });
 //当有任何一个客户端连接到服务器时，给这个客户端推送一条消息
 wsServer.on("connection", function (websocket) {
+    //  连接成功
     websocket.send("hello world welcome");
+    // 接收客户端发来消息
     websocket.on('message', function (message) {
         console.log(message);
         websocket.send("ok");
--- helloword.ts
+++ helloword.ts
@@ -2,8 +2,9 @@ import {Server} from 'ws';
 const wsServer = new Server({port:8081});
 //当有任何一个客户端连接到服务器时，给这个客户端推送一条消息
 wsServer.on("connection",websocket => {
+    //  连接成功
     websocket.send("hello world welcome");
-
+    // 接收客户端发来消息
     websocket.on('message',message => {
         console.log(message)
         websocket.send("ok");
